90b509ad990f523a8a2fa59a9ea49ce81bdf6a5b
Joan Lustenberger <lustenberger.joan@outlook.com>
2016-06-21 23:27:00 +0200

    Import from Sublime

--- src/extension.ts
+++ src/extension.ts
@@ -19,6 +19,18 @@ export function activate(context: vscode.ExtensionContext) {
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
+
+        var editor = vscode.window.activeTextEditor;
+
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
+
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Selected characters: ' + text.length);
     });
 
     context.subscriptions.push(disposable);
